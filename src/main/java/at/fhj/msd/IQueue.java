7bedfdcd2e947004eed388933a3efeba431f9381
Liyu Wu <liyu.wu@edu.fh-joanneum.at>
2025-04-20 20:30:27 +0200

    Modify class Drink to interface as require
    Replaced 'public abstract class Dring' with 'public interfact Drink'. Removed 'protected String name' and constructor.

--- src/main/java/at/fhj/msd/IQueue.java
+++ src/main/java/at/fhj/msd/IQueue.java
@@ -1,34 +1,30 @@
 package at.fhj.msd;
-// fastly written (not really nice) comments you should adapt
+// define an interface IQueue with the following methods
+// offer(String obj): boolean
+
 public interface IQueue {
-  /** 
-   * I am a really bad comment
-   * 
-   * fasdfadsffdsadsf
-   * 
-   * fdsasdfadsf
-   * 
-   *  add object to queue, true if okay 
-   * 
-   * i fell asleep on my keyboard...
-   * 
-   * */ 
-  public abstract boolean offer(String obj);
+/**
+ * Adds an element to the queue.
+ *
+ * @param obj the string to add
+ * @return true if the element was added successfully
+ */
+  boolean offer(String obj);
 
   //returns + del 1st element; null if nothing in there
    //
-  public abstract String poll();
+  String poll();
 
   /* 
   same as poll, if there is nothing in there error NoSuchElementException
    */
-  public abstract String remove();
+  String remove();
 
   // 1st element without dlete, otherwise null
    
-  public abstract String peek();
+  String peek();
 
   // element is to peek what remove is to poll
   
-  public abstract String element();
+  String element();
 }
