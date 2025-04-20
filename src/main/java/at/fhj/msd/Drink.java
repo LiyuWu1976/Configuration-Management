3e957bc2e4695031796499c9bda54ebda7973639
Liyu Wu <liyu.wu@edu.fh-joanneum.at>
2025-04-20 20:09:00 +0200

    Modify class Drink to interface as require
    Replaced 'public abstract class Dring' with 'public interfact Drink'. Removed 'protected String name' and constructor.

--- src/main/java/at/fhj/msd/Drink.java
+++ src/main/java/at/fhj/msd/Drink.java
@@ -3,45 +3,34 @@ package at.fhj.msd;
 /**
  * Abstract base class for concrete drink classes
  */
-public abstract class Drink {
+public interface Drink {
+
+
     /**
-     * name of the drink
-     */
-    protected String name;
-    /**
-     * Creates a Drink object with given name
      *
-     * @param name name of the drink
+     * @param name return the name of the drink
      *
      */
-    public Drink(String name) {
-        this.name = name;
-    }
+    String getName();
+
     /**
      * Calculates and returns volume of drink
      *
      * @return the volume of drink in liter
      */
-    public abstract double getVolume();
+    double getVolume();
     /**
      * Calculates and returns the alcohol percentage
      *
      * @return alcohol volume percent (e.g. 50)
      */
-    public abstract double getAlcoholPercent();
+    double getAlcoholPercent();
+
+
     /**
      * Gives information if drink is alcoholic or not
      *
      * @return true when alcoholic liquids are present, otherwise false
      */
-    public abstract boolean isAlcoholic();
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    boolean isAlcoholic();
 }
